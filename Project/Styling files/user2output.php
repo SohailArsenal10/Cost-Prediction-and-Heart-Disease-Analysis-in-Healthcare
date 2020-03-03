e6f6fbb73c84f02f0428b7dfb58b7d67ec4ec4d1
Syed Sohail Ahmed <sihailahmed1999@gmail.com>
2020-03-03 19:35:48 +0530

    Add files via upload

---
+++ Project/Styling files/user2output.php
@@ -0,0 +1,9 @@
+<head>
+    
+<style>
+ 
+</style>
+
+</head>
+<a href = "https://public.tableau.com/profile/syed.sohail.ahmed#!/vizhome/HeartDiseaseAnalysis/Sheet1"  target = "_blank"><button class="but"> Click here to view single sheet!!</button></a>
+<a href = "https://public.tableau.com/profile/syed.sohail.ahmed#!/vizhome/IndividualHeartParameterAnalysis/Story1"  target = "_blank"><button class="but"> Click here to view report!!</button></a>
